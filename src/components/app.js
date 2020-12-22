22a939e81ad90ff41898185183822c81f9247af4
hadichloun <hadi_960@hotmail.com>
2020-12-22 11:54:10 -0600

    First commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from "./portfolio/portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className="app">
         <h1>Hadi Chloun portfolio</h1>
-        <div>
        <div>{moment().format('MMM do YYYY, h:mm:ss a')}</div>
+       <PortfolioContainer />
        </div>
-      </div>
+      
     );
   }
 }
